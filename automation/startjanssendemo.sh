a47c0bb5e0652550e5c1893f05071346efda10b4
Amro Misbah <amromisba7@gmail.com>
2024-12-07 19:20:00 +0200

    Update startjanssendemo.sh

--- automation/startjanssendemo.sh
+++ automation/startjanssendemo.sh
@@ -40,9 +40,9 @@ sudo microk8s.enable dns registry ingress hostpath-storage helm3
 sudo microk8s kubectl get daemonset.apps/nginx-ingress-microk8s-controller -n ingress -o yaml | sed -s "s@ingress-class=public@ingress-class=nginx@g" | microk8s kubectl apply -f -
 sudo snap alias microk8s.kubectl kubectl
 sudo snap alias microk8s.helm3 helm
+sudo microk8s config | sudo tee ~/.kube/config > /dev/null
 KUBECONFIG=~/.kube/config
-export KUBECONFIG
-sudo microk8s.kubectl create namespace jans || echo "namespace exists"
+sudo microk8s.kubectl create namespace jans --kubeconfig="$KUBECONFIG" || echo "namespace exists"
 
 if [[ $INSTALL_ISTIO == "true" ]]; then
   sudo microk8s.kubectl label ns jans istio-injection=enabled
@@ -55,8 +55,8 @@ fi
 
 PERSISTENCE_TYPE="sql"
 if [[ $JANS_PERSISTENCE == "MYSQL" ]]; then
-  sudo microk8s.kubectl get po
-  sudo helm install my-release --set auth.rootPassword=Test1234#,auth.database=jans -n jans oci://registry-1.docker.io/bitnamicharts/mysql
+  sudo microk8s.kubectl get po --kubeconfig="$KUBECONFIG"
+  sudo helm install my-release --set auth.rootPassword=Test1234#,auth.database=jans -n jans oci://registry-1.docker.io/bitnamicharts/mysql --kubeconfig="$KUBECONFIG"
   cat << EOF > override.yaml
 config:
   countryCode: US
@@ -74,8 +74,8 @@ config:
 EOF
 fi
 if [[ $JANS_PERSISTENCE == "PGSQL" ]]; then
-  sudo microk8s.kubectl get po
-  sudo helm install my-release --set auth.postgresPassword=Test1234#,auth.database=jans -n jans oci://registry-1.docker.io/bitnamicharts/postgresql
+  sudo microk8s.kubectl get po --kubeconfig="$KUBECONFIG"
+  sudo helm install my-release --set auth.postgresPassword=Test1234#,auth.database=jans -n jans oci://registry-1.docker.io/bitnamicharts/postgresql --kubeconfig="$KUBECONFIG"
   cat << EOF > override.yaml
 config:
   countryCode: US
@@ -166,7 +166,7 @@ nginx-ingress:
 EOF
 sudo helm repo add janssen https://docs.jans.io/charts
 sudo helm repo update
-sudo helm install janssen janssen/janssen -n jans -f override.yaml --version="$JANS_VERSION"
+sudo helm install janssen janssen/janssen -n jans -f override.yaml --version="$JANS_VERSION" --kubeconfig="$KUBECONFIG"
 cat << EOF > testendpoints.sh
 echo -e "Testing openid-configuration endpoint.. \n"
 curl -k https://$JANS_FQDN/.well-known/openid-configuration
@@ -176,6 +176,6 @@ echo -e "Testing fido2-configuration endpoint.. \n"
 curl -k https://$JANS_FQDN/.well-known/fido2-configuration
 EOF
 echo "Waiting for Janssen to come up. Please do not cancel out. This can take ~5 minutes."
-sudo microk8s.kubectl -n jans wait --for=condition=available --timeout=300s deploy/janssen-auth-server || echo "Couldn't find deployment running tests anyways..."
+sudo microk8s.kubectl -n jans wait --for=condition=available --timeout=300s deploy/janssen-auth-server --kubeconfig="$KUBECONFIG" || echo "Couldn't find deployment running tests anyways..."
 sudo bash testendpoints.sh
 echo -e "You may re-execute the command 'bash testendpoints.sh' to do a quick test to check the openid-configuration endpoint."
