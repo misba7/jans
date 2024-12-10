b4aa9fb694be59c3170601811bbecece215ccc52
Amro Misbah <amromisba7@gmail.com>
2024-12-10 18:00:57 +0200

    Update startjanssendemo.sh

--- automation/startjanssendemo.sh
+++ automation/startjanssendemo.sh
@@ -37,8 +37,6 @@ fi
 
 sudo apt-get update
 sudo pip3 install requests --upgrade
-sudo pip3 install shiv
-sudo snap install microk8s --classic
 sudo microk8s.status --wait-ready
 sudo microk8s.enable dns registry ingress hostpath-storage helm3
 sudo microk8s kubectl get daemonset.apps/nginx-ingress-microk8s-controller -n ingress -o yaml | sed -s "s@ingress-class=public@ingress-class=nginx@g" | microk8s kubectl apply -f -
